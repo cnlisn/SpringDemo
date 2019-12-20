e2677df39a798e4074d67f1fecf5b9819a4611a9
cnlisn <cnlishan@163.com>
2019-12-20 11:11:44 +0800

    添加系统发送邮件功能

---
+++ src/main/java/com/lisn/demo/core/constant/MailConstant.java
@@ -0,0 +1,17 @@
+package com.lisn.demo.core.constant;
+
+/**
+ * 邮件常量类
+ */
+public class MailConstant {
+
+    /**
+     * 注册的模板名称
+     */
+    public static final String RETGISTEREMPLATE = "register";
+
+    /**
+     * 模板存放的路径
+     */
+    public static final String TEMPLATEPATH = "src/test/java/resources/template/mail";
+}
